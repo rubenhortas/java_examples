ae41d8bcef8d7f9cbf359488ae4cff35f50b4b7d
ruben <ruben>
2015-05-09 09:01:04 +0200

    Added

---
+++ src/finalArguments/MyClass.java
@@ -0,0 +1,19 @@
+/**
+ * Java 
+ * 
+ * @author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com.es>
+ * @contact: rubenhortas at gmail.com
+ * @web:     http://www.rubenhortas.blogspot.com.es
+ * @github:  http://github.com/rubenhortas
+ * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+ * @file:    MyClass.java
+ */
+
+package finalArguments;
+
+public class MyClass {
+
+    public void doSomething() {
+	System.out.println("do something");
+    }
+}
