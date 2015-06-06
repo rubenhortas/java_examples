159b79dcf530e890a478df5446096991beb7690e
ruben <ruben>
2015-06-06 14:36:49 +0200

    Added

---
+++ src/multiple_inheritance/IShip.java
@@ -0,0 +1,16 @@
+/**
+ * Java 
+ * 
+ * @author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com.es>
+ * @contact: rubenhortas at gmail.com
+ * @web:     http://www.rubenhortas.blogspot.com.es
+ * @github:  http://github.com/rubenhortas
+ * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+ * @file:    IShip.java
+ */
+
+package multiple_inheritance;
+
+public interface IShip {
+    public void canFloat();
+}
