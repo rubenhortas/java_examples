03510e7937bdeeea580a1b79562a67d1bf655a41
ruben <ruben>
2015-06-07 00:14:28 +0200

    Added

---
+++ src/extendInterfacesWithInheritance/Batmobile.java
@@ -0,0 +1,34 @@
+/**
+ * Java 
+ * 
+ * @author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com.es>
+ * @contact: rubenhortas at gmail.com
+ * @web:     http://www.rubenhortas.blogspot.com.es
+ * @github:  http://github.com/rubenhortas
+ * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+ * @file:    Batmobile.java
+ */
+
+package extendInterfacesWithInheritance;
+
+public class Batmobile implements IBatmobile {
+
+    @Override
+    public void shoot() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void run() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void startJetEngine() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
