8797e83fde7421cb446d7298b2b833dea9083377
ruben <ruben.hortas+github@gmail.com>
2015-07-02 19:47:28 +0200

    Fixed package name

--- src/lists/ListsExample.java
+++ src/lists/ListsExample.java
@@ -6,7 +6,7 @@
  * @web:     http://www.rubenhortas.blogspot.com.es
  * @github:  http://github.com/rubenhortas
  * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
- * @file:    Instrument.java
+ * @file:    ListExample.java
  */
 
 package lists;
