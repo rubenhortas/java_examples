dd7c8d1328c27728d735665f96414a4d4cf81c91
ruben <ruben.hortas+github@gmail.com>
2015-07-04 21:53:16 +0200

    Added example with Collection objects.

--- src/foreach/Foreach.java
+++ src/foreach/Foreach.java
@@ -12,6 +12,9 @@
 package foreach;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Foreach {
@@ -27,6 +30,16 @@ public class Foreach {
 		
 		for(String device : electronicDevices) {
 			System.out.println(device);
+		}
+		
+		System.out.println();
+		
+		// Foreach also works with any Collection object
+		Collection<String> collectionString = new LinkedList<String>();
+		Collections.addAll(collectionString, "Follow the white rabbit".split(" "));
+		
+		for(String str: collectionString) {
+			System.out.print("'" + str + "' ");
 		}
 	}
 }
