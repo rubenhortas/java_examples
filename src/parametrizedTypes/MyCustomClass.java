8c1f78f2a97a68fe168a64ca8e75807971babaa9
rubenhortas <rubenhortas@gmail.com>
2015-10-08 21:08:22 +0200

    Added

---
+++ src/parametrizedTypes/MyCustomClass.java
@@ -0,0 +1,20 @@
+/**
+ * Java 
+ * 
+ * @author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com.es>
+ * @contact: rubenhortas at gmail.com
+ * @web:     http://www.rubenhortas.blogspot.com.es
+ * @github:  http://github.com/rubenhortas
+ * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+ * @file:    MyCustomClass.java
+ */
+
+package parametrizedTypes;
+
+public class MyCustomClass {
+
+	public MyCustomClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
