6e9ffa5add1aaec1fea4d08b55cc34e23991e1fa
rubenhortas <rubenhortas@gmail.com>
2016-01-23 13:30:16 +0100

    Added EnumSet example

---
+++ src/enums/EnumSetEx.java
@@ -0,0 +1,33 @@
+/**
+ * Java 
+ * 
+ * @author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com.es>
+ * @contact: rubenhortas at gmail.com
+ * @web:     http://www.rubenhortas.blogspot.com.es
+ * @github:  http://github.com/rubenhortas
+ * @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+ * @file:    EnumSetEx.java
+ */
+
+package enums;
+
+import java.util.EnumSet;
+
+public class EnumSetEx {
+	public static void main(String[] args) {
+		// Empty set
+		EnumSet<Food> cupboard = EnumSet.noneOf(Food.class);
+		cupboard.add(Food.CANDIES);
+		cupboard.add(Food.POPCORN);
+		cupboard.add(Food.CHIPS);
+		System.out.println(cupboard);
+		
+		// Removing all
+		cupboard.removeAll(EnumSet.range(Food.CHIPS, Food.CANDIES));
+		System.out.println(cupboard);
+		
+		// Adding all
+		cupboard = EnumSet.allOf(Food.class);
+		System.out.println(cupboard);
+	}
+}
