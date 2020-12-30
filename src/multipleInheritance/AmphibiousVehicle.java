81d2aa92b31f7add79b5a99c42cca27ee1fe1ee1
ruben <ruben.hortas@gmail.com>
2020-12-30 22:40:56 +0100

    Migrated to intellij

---
+++ src/multipleInheritance/AmphibiousVehicle.java
@@ -0,0 +1,21 @@
+package multipleInheritance;
+
+/**
+ * In Java you can do Multiple Inheritance implementing interfaces. If you do
+ * inherit from a non-interface, you can inherit from only one.
+ * You can have as many interfaces as you want. You can upcast to each
+ * interface.
+ */
+@SuppressWarnings("unused")
+public class AmphibiousVehicle implements ICar, IShip {
+    @Override
+    public void canFloat() {
+        System.out.println("I can float");
+    }
+
+    @Override
+    public void canRun() {
+        System.out.println("I can run");
+    }
+
+}
