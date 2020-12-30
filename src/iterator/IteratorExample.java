72c73ed2ec0db2c724c191d9627de94b7c844d6a
ruben <ruben.hortas@gmail.com>
2020-12-30 23:39:08 +0100

    Suppressed warning

--- src/iterator/IteratorExample.java
+++ src/iterator/IteratorExample.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class IteratorExample {
+    @SuppressWarnings("WhileLoopReplaceableByForEach")
     public static void main(String[] args) {
         List<Item> itemList = Item.arrayList(4);
         Iterator<Item> it = itemList.iterator();
