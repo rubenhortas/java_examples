82425b5292851ecd66f0cb67072a24b048b0ced3
rubenhortas <rubenhortas@gmail.com>
2022-05-17 18:56:43 +0200

    Testing token

--- src/arraysExample/ArrayExample.java
+++ src/arraysExample/ArrayExample.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class ArrayExample {
     @SuppressWarnings("MismatchedReadAndWriteOfArray")
+
     public static void main(String[] args) {
         // Arrays vs Lists
         // Arrays are more efficient, but lists automatically allocates more space.
