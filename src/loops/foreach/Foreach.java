6d488d0bd5bbcbdc60d54330f6764f5cfd064409
rubenhortas <rubenhortas@gmail.com>
2022-08-02 17:02:34 +0200

    Refactoring

--- src/loops/foreach/Foreach.java
+++ src/loops/foreach/Foreach.java
@@ -2,30 +2,34 @@ package loops.foreach;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Foreach {
+
+    private static final List<String> ELECTRONIC_DEVICES = new ArrayList<>() {{
+        add("router");
+        add("raspberry pi");
+        add("tv");
+        add("hard disk");
+    }};
+
+    private static final Collection<String> MATRIX_MESSAGE = new LinkedList<>() {{
+        add("Follow");
+        add("the");
+        add("white");
+        add("rabbit");
+    }};
+
     public static void main(String[] args) {
-        List<String> electronicDevices = new ArrayList<>();
-
-        electronicDevices.add("router");
-        electronicDevices.add("raspberry pi");
-        electronicDevices.add("tv");
-        electronicDevices.add("hard disk");
-
-        for (String device : electronicDevices) {
+        for (String device : ELECTRONIC_DEVICES) {
             System.out.println(device);
         }
 
         System.out.println();
 
         // Foreach also works with any Collection object
-        Collection<String> collectionString = new LinkedList<>();
-        Collections.addAll(collectionString, "Follow the white rabbit".split(" "));
-
-        for (String str : collectionString) {
+        for (String str : MATRIX_MESSAGE) {
             System.out.print("'" + str + "' ");
         }
     }
