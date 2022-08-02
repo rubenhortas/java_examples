d38a458044c39406bbf630c67b8b9aeea6a578d9
rubenhortas <rubenhortas@gmail.com>
2022-08-02 16:45:20 +0200

    Refactoring

--- src/classes/inheritance/upcasting/Instrument.java
+++ src/classes/inheritance/upcasting/Instrument.java
@@ -2,10 +2,10 @@ package classes.inheritance.upcasting;
 
 public class Instrument {
 	public void play() {
-		System.out.println("playing...");
+		System.out.println("Playing...");
 	}
 
-	static void tune(Instrument i) {
-		i.play();
+	static void tune(Instrument instrument) {
+		instrument.play();
 	}
 }
--- src/classes/inheritance/upcasting/Wind.java
+++ src/classes/inheritance/upcasting/Wind.java
@@ -2,9 +2,8 @@ package classes.inheritance.upcasting;
 
 // Example of classes.inheritance.upcasting.
 public class Wind extends Instrument {
-    /* Tune accepts an instrument reference. Wind object is an Instrument
-       object. Upcasting is always safe, but the class interface can lose
-       methods. */
+    /*  Tune accepts an instrument reference. Wind object is an Instrument object.
+        Upcasting is always safe, but the class interface can lose methods. */
     public static void main(String[] args) {
         Wind flute = new Wind();
         tune(flute); // Upcasting
