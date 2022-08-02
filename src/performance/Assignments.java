1806cb886d173a0cfa83a59afa61a3b96a0302e7
rubenhortas <rubenhortas@gmail.com>
2022-08-02 23:18:54 +0200

    Refactoring

--- src/performance/Assignments.java
+++ src/performance/Assignments.java
@@ -4,8 +4,8 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class Assignments {
-    private static final int ITERATIONS = Integer.MAX_VALUE;
-    private static final int ASSIGNMENTS = Integer.MAX_VALUE;
+    private static final int ITERATIONS = 100000;
+    private static final int ASSIGNMENTS = 100000;
 
     public static void main(String[] args) {
         Instant start; // Requires java 8.
@@ -17,15 +17,15 @@ public class Assignments {
         start = Instant.now();
         assignInsideTheLoop();
         finish = Instant.now();
-        time = Duration.between(start, finish).toSeconds();
-        System.out.printf("Assigning the variable inside the loop: %d seconds.\n", time);
+        time = Duration.between(start, finish).toMillis();
+        System.out.printf("Assigning the variable inside the loop: %d milliseconds.\n", time);
 
         // Assigning the variable outside the loop is slightly faster.
         start = Instant.now();
         assignOutsideTheLoop();
         finish = Instant.now();
-        time = Duration.between(start, finish).toSeconds();
-        System.out.printf("Assigning the variable outside the loop: %d seconds.", time);
+        time = Duration.between(start, finish).toMillis();
+        System.out.printf("Assigning the variable outside the loop: %d milliseconds.", time);
     }
 
     private static void assignInsideTheLoop() {
