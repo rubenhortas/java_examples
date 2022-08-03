ae08b348cfd7b0dc6d84ff82b0767d065a7f6341
rubenhortas <rubenhortas@gmail.com>
2022-08-03 23:16:52 +0200

    Refactoring

--- src/types/datatypes/hashes/hashmap/HashMap.java
+++ src/types/datatypes/hashes/hashmap/HashMap.java
@@ -4,19 +4,20 @@ import java.util.Map;
 import java.util.Random;
 
 public class HashMap {
-    @SuppressWarnings("Java8MapApi")
     public static void main(String[] args) {
         Random random = new Random(47);
         Map<Integer, Integer> intMap = new java.util.HashMap<>();
+        int randomValue;
+        Integer frequency;
 
         for (int i = 0; i < 100; i++) {
-            int randomInt = random.nextInt(20);
-            Integer frequency = intMap.get(randomInt);
+            randomValue = random.nextInt(20);
+            frequency = intMap.get(randomValue);
 
             if (frequency == null) {
-                intMap.put(randomInt, 1);
+                intMap.put(randomValue, 1);
             } else {
-                intMap.put(randomInt, frequency + 1);
+                intMap.put(randomValue, frequency + 1);
             }
         }
 
