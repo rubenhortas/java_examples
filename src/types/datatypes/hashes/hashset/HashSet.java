6d9ba1ec4ba9bfdb05a2952d23ed5ae47e7b38ce
rubenhortas <rubenhortas@gmail.com>
2022-08-04 10:54:48 +0200

    Refactoring

--- src/types/datatypes/hashes/hashset/HashSet.java
+++ src/types/datatypes/hashes/hashset/HashSet.java
@@ -4,9 +4,10 @@ import java.util.Random;
 import java.util.Set;
 
 public class HashSet {
+    private static final Random random = new Random(47);
+
     // A Set refuses to hold more than one instance of each object value.
     public static void main(String[] args) {
-        Random random = new Random(47);
         Set<Integer> intSet = new java.util.HashSet<>();
 
         for (int i = 0; i <= 100; i++) {
